75df434a801625d1c503718b9d9344656ebf7924
B9ary01 <bishalrai111@yahoo.com>
2022-08-17 12:10:57 +0100

    insert query

--- dbConfig/init.js
+++ dbConfig/init.js
@@ -2,4 +2,12 @@ const { Pool } = require("pg");
 
 const pool = new Pool();
 
+pool.query('INSERT INTO users(name,email,password) VALUES("admin","admin@gmail.com","password")', (err, res) => {
+    if (err) {
+      throw err
+    }
+    console.log('added admin:', res.rows[0])
+  })
+
+
 module.exports = pool;
